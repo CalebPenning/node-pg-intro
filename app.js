691caab37f336d49ae551252fa240cf312ea0983
calebpenning <calebpenning@mail.com>
2021-05-04 20:34:16 -0700

    initial commmit. have not connected to db yet

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const ExpressError = require('./expressError')
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    const err = new ExpressError("Not Found", 404)
+
+    // pass error to middleware
+
+    return next(err)
+})
+
+app.use((err, req, res, next) => {
+    let status = err.status || 500
+
+    return res.status(status).json({
+        error: {
+            message: err.message,
+            status: status
+        }
+    })
+})
+
+module.exports = app
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+/** Routes for users of our express app */ 
+
+const express = require("express")
+const router = new express.Router()
+
+module.exports = router
