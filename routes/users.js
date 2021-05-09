62fff289dd038d341750b108f504d86006422ff6
calebpenning <calebpenning@mail.com>
2021-05-08 23:28:00 -0700

    added some test data. about to implement tests

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const express = require("express")
 const router = new express.Router()
 const db = require("../db")
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
     // db calls are asynchronous!!
     try { 
         const results = await db.query(
@@ -14,6 +14,83 @@ router.get("/", async (req, res) => {
         return next(e)
     }
 })
+
+router.post("/", async (req, res, next) => {
+    try {
+        const { name, type } = req.body
+        // Though the insert statement works and creates an employee,
+        // it does not return any information to use.
+        // The RETURNING clause is added at the end to a. end the statement and
+        // b. return the information about the inserted data
+        const results = await db.query(`INSERT INTO users (name, type) VALUES ($1, $2) RETURNING id, name, type`, [name, type])
+        return res.status(201).json(results.rows)
+    }
+    catch(e) {
+        return next(e)
+    }
+})
+
+router.patch("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const {name, type} = req.body
+        const results = await db.query(
+            `UPDATE users 
+            SET name=$1, type=$2 
+            WHERE id=$3 
+            RETURNING id, name, type`, [name, type, id])
+        return res.send(results.rows[0])
+    } 
+
+    catch(e) {
+        return next(e)
+    }
+})
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const results = await db.query(
+            `DELETE FROM users
+            WHERE id = $1`, [req.params.id])
+            return res.json({ success: {message: "Your query was successful."}})
+    }
+
+    catch(e) {
+        return next(e)
+    }
+})
+
+router.get("/search", async (req, res, next) => {
+    try {
+        const { type } = req.query.type
+        const results = await db.query(`SELECT * FROM users WHERE type=$1`)
+        return res.json(results.rows)
+    } catch(e) {
+        return res.json({
+            error: {
+                msg: e
+            }
+        })
+    }
+})
+
+router.get("/good-search",
+    async (req, res, next) => {
+        try {
+            const type = req.body.type
+
+            const results = await db.query(
+                `SELECT id, name, type
+                FROM users
+                WHERE type=$1`, [type])
+
+            return res.json(results.rows)
+        }
+
+        catch(e) {
+            return next(e)
+        }
+    })
 
 
 module.exports = router
